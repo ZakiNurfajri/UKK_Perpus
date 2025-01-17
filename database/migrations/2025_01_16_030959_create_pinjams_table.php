dd23dcf2644f29dbe28bcf5d39990c1efd10c8ee
ZakiNurfajri <zakiwidyanto@gmail.com>
2025-01-17 13:26:47 +0700

    schema db clear

--- database/migrations/2025_01_16_030959_create_pinjams_table.php
+++ database/migrations/2025_01_16_030959_create_pinjams_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('bukus', function (Blueprint $table) {
+        Schema::create('pinjams', function (Blueprint $table) {
             $table->id();
-            $table->string('judul');
-            $table->foreignid('kategori_id')->constrained('kategori_bukus');
-            $table->string('kode_buku');
-            $table->string('penulis');
-            $table->string('penerbit');
-            $table->string('tanggal_terbit');
+            $table->foreignid('user_id')->constrained('users');
+            $table->foreignid('buku_id')->constrained('bukus');
+            $table->date('tgl-pinjam');
+            $table->date('tgl-kembali');
+            $table->varchar('status')->default(null);
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bukus');
+        Schema::dropIfExists('pinjams');
     }
 };
